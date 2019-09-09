e6e41acb57379da10143d531bb325ea3e8f80375
Tarun Gorai <goraitarunkumar@gmail.com>
2019-09-09 22:32:47 +0530

    passing parameter to route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { ServersService } from './servers/servers.service';
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'users', component: UsersComponent },
+  { path: 'users/:id', component: UserComponent },
   { path: 'servers', component: ServersComponent },
 ];
 

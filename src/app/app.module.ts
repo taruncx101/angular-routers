d67f6aa161132a29c2a7fbf6c7ccc63a91c150d0
Tarun Gorai <goraitarunkumar@gmail.com>
2019-09-10 08:25:04 +0530

    add routes in servers and users list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ const appRoutes: Routes = [
   { path: 'users', component: UsersComponent },
   { path: 'users/:id/:name', component: UserComponent },
   { path: 'servers', component: ServersComponent },
+  { path: 'servers/:id', component: ServerComponent },
   { path: 'servers/:id/edit', component: ServersComponent },
 ];
 
